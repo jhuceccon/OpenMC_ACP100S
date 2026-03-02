0b8f6615c44060855015f12e1707aa516e4a60b3
Thalles Campagnani <thallescampagnani@gmail.com>
2026-03-02 20:29:13 -0300

    Deletando 'for' desnecessário

--- runACP100S.py
+++ runACP100S.py
@@ -19,15 +19,11 @@ libACP100S.simu = False
 libACP100S.plotar = True
 libACP100S.verbose = True
 
-ini=3
-fin=4
-passo=1
-for enr in range(ini,fin,passo):
-    libACP100S.mkdir(nome=f"resultados_UO2_enriquecimento_{enr}", data=False, voltar=False if enr == ini else True) 
-    reator = libACP100S.modelo()
-    reator.materiais(enriquecimento=enr/100)
-    reator.geometria(plotar_interno=False)
-    reator.configuracoes(particulas=1000)
-    reator.plotar(width=(200,200),pixels=(5000,5000))
-    reator.plotar(filename="zoom1", width=(25,25),pixels=(5000,5000))
-    reator.simular()
+libACP100S.mkdir(nome=f"resultados", data=False) 
+
+reator = libACP100S.modelo()
+reator.geometria(plotar_interno=True)
+reator.configuracoes(particulas=1000)
+reator.plotar(width=(200,200),pixels=(5000,5000))
+reator.plotar(filename="zoom1", width=(25,25),pixels=(5000,5000))
+reator.simular()
